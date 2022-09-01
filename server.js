48586e5ef357c691deafbc7f4af2f2a13df729e2
Rasmus Enholm <rasmus.enholm@hotmail.com>
2022-09-01 12:24:39 +0300

    fixed public path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const http = require("http")
 const path = require("path")
-const publicPath = path.join(__dirname, "..", "public")
+const publicPath = path.join(__dirname, "./", "public")
 
 const app = express()
 
